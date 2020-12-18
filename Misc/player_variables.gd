172627a8848245847b3e23493ba8ce1a5a200bc4
Lucas Bergognon <lucas.bergognon@gmail.com>
2020-12-18 16:53:56 +0100

    Reset level settings and chopper life modification

--- Misc/player_variables.gd
+++ Misc/player_variables.gd
@@ -3,10 +3,10 @@ extends Node
 
 var coin_per_second: float = 1.0
 var coin: float
-var base_coin: float = 300
+var base_coin: float = 30
 
 var score: int = 0
-var level: int = 10
+var level: int = 0
 
 func level_cleared() -> void:
 	coin_per_second += min(1.0, float(level) / 10.0)

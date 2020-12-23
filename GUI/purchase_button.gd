cca7fc213c97ec7c24a1453b1212b4ba5deaf0bc
Lucas Bergognon <lucas.bergognon@gmail.com>
2020-12-23 09:50:08 +0100

    Add a confirmation sound when clicking a button

--- GUI/purchase_button.gd
+++ GUI/purchase_button.gd
@@ -10,7 +10,7 @@ onready var price_label = $MarginContainer/VBoxContainer/Price
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	name_label.text = purchase.title
-	price_label.text = "%d" % purchase.price	
+	price_label.text = "%d" % purchase.price
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -20,3 +20,5 @@ func _process(_delta):
 
 func _on_PurchaseButton_pressed():
 	emit_signal("item_purchased", purchase)
+	$AudioStreamPlayer.play()
+
